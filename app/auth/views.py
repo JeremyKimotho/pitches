67633dff42a7203558e5f2a236f786b28a6cdc5f
Jeremy Kimotho <jeremykimotho@yahoo.com>
2018-11-16 21:04:31 +0300

    i have written the registration and login forms, now writing html templates

--- app/auth/views.py
+++ app/auth/views.py
@@ -25,3 +25,18 @@ def register():
   form = RegistrationForm()
   if form.validate_on_submit():
     user = User(email=form.email.data, username=form.username.data, password=form.password.data, fname=form.fname.data, lname=form.lname.data)
+
+    db.session.add(user)
+    db.session.commit()
+
+    mail_message('Welcome to Jitches', 'email/welcome_user', user.email, user=user)
+    return redirect(url_for('auth.login'))
+    title='New Account'
+
+  return render_template('auth/register.html', registration_form=form)
+
+@auth.route('/logout')
+@login_required
+def logout():
+  logout_user()
+  return redirect(url_for('auth.login'))

04140d4c57706ddc11478b9770acb3b6ac1f2933
Jeremy Kimotho <jeremykimotho@yahoo.com>
2018-11-16 20:45:00 +0300

    i have begun writing the login and registration forms

---
+++ app/main/views.py
@@ -0,0 +1,6 @@
+from flsak import render_template, request, redirect, url_for, abort
+from . import main
+from ..models import User
+from flask_login import login_required, current_user
+from .. import db
+import markdown2

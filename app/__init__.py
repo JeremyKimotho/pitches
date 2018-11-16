d60e4e3a26b0cde976bcebf303a414d1380fe704
Jeremy Kimotho <jeremykimotho@yahoo.com>
2018-11-16 21:50:00 +0300

    corrected the init.py to remove the uploads feature because it is needless

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_uploads import UploadSet, configure_uploads
+from flask_uploads import UploadSet
 from flask_mail import Mail
 from flask_simplemde import SimpleMDE
 
@@ -35,8 +35,6 @@ def create_app(config_name):
 
   from .auth import auth as auth_blueprint
   app.register_blueprint(auth_blueprint, url_prefix = '/authenticate')
-
-  configure_uploads(app)
 
   simple.init_app(app)
 
